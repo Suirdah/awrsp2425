e82c6646413d18926165fe027f88b3a2074be1c3
= <lemur123123@gmail.com>
2024-11-07 19:09:12 +0100

    Main fix

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        feature/ - gałęzie dedykowane dla nowych funkcjonalności
-        bugfix/ - gałęzie dedykowane dla napraw błędów
-        hotfix/ - gałęzie dedykowane dla nowych wersji naprawy
-        release/ - gałęzie dedykowane dla wydawania nowej wersji
-        np
-        feature/Project_ZM_4567_FormUpdate
-         */
         System.out.println("Hello world!");
         for(int i = 0; i<1 ; i++){
 
